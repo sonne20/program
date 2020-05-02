73797faa6b041b35a065260417315636996f7a86
alizenit <64712419+alizenit@users.noreply.github.com>
2020-05-03 02:05:25 +0300

    Add files via upload
    deneme1

---
+++ ilk.py
@@ -0,0 +1,18 @@
+import time
+import re
+
+for i in range(5):
+	print('Hello Kemal')
+	time.sleep(1)
+	
+	
+line = "Cats are smarter than dogs"
+
+matchObj = re.match( r'(.*) are (.*?) .*', line, re.M|re.I)
+
+if matchObj:
+   print("matchObj.group() : ", matchObj.group())
+   print("matchObj.group(1) : ", matchObj.group(1))
+   print("matchObj.group(0) : ", matchObj.group(2))
+else:
+   print('No match!!')
